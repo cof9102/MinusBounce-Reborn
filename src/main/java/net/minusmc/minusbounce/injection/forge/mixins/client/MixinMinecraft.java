183b17f950a733e5eb3945a86c1f48b83a6fb6fb
The Smart Cat <nguyendinhgiabao230311@gmail.com>
2025-08-09 01:11:46 +0700

    kcj
    edit tren dth

--- src/main/java/net/minusmc/minusbounce/injection/forge/mixins/client/MixinMinecraft.java
+++ src/main/java/net/minusmc/minusbounce/injection/forge/mixins/client/MixinMinecraft.java
@@ -198,7 +198,7 @@ public abstract class MixinMinecraft {
 
     @Inject(method = "createDisplay", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/Display;setTitle(Ljava/lang/String;)V", shift = At.Shift.AFTER))
     private void createDisplay(CallbackInfo callbackInfo) {
-        Display.setTitle(MinusBounce.CLIENT_NAME + " build " + MinusBounce.CLIENT_VERSION);
+        Display.setTitle(MinusBounce.CLIENT_NAME + " Reborn " + MinusBounce.CLIENT_VERSION);
     }
 
     @Inject(method = "loadWorld(Lnet/minecraft/client/multiplayer/WorldClient;Ljava/lang/String;)V", at = @At("HEAD"))
